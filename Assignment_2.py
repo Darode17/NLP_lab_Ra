7b8ea4e530cbf5a32d1730c8a42b19676bfe5df0
Radhika Darode <113372152+Darode17@users.noreply.github.com>
2023-10-19 09:02:39 +0530

    Update Assignment_2.py

--- Assignment_2.py
+++ Assignment_2.py
@@ -2,17 +2,17 @@
 #Name:Radhika Santosh Darode
 ##Roll No:16
 #Batch:B1
-#Title:Assignment to implement Bag of Words.
+#Title:Assignment to implement Bag of Words and TFIDF using Gensim library.
 
 import gensim
 from gensim import corpora
-
-text1 = ["""Natural language processing is a machine learning technology that gives 
-        computers the ability to interpret, manipulate, and comprehend human language."""]
+from gensim.utils import simple_preprocess
+text1 = ["""The Dog sat
+         The Dog sat in the hat
+         The Dog sat with the hat."""]
 
 tokens1 = [[item for item in line.split()] for line in text1]
 g_dict1 = corpora.Dictionary(tokens1)
-
 
 print("The dictionary has: " +str(len(g_dict1)) + " tokens\n")
 print(g_dict1.token2id)
@@ -20,7 +20,32 @@ print(g_dict1.token2id)
 g_bow =[g_dict1.doc2bow(token, allow_update = True) for token in tokens1]
 print("Bag of Words : ", g_bow)
 
-"""The dictionary has: 20 tokens
+text = ["""The Dog sat
+         The Dog sat in the hat
+         The Dog sat with the hat."""]
 
-{'Natural': 0, 'a': 1, 'ability': 2, 'and': 3, 'comprehend': 4, 'computers': 5, 'gives': 6, 'human': 7, 'interpret,': 8, 'is': 9, 'language': 10, 'language.': 11, 'learning': 12, 'machine': 13, 'manipulate,': 14, 'processing': 15, 'technology': 16, 'that': 17, 'the': 18, 'to': 19}
-Bag of Words :  [[(0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1), (8, 1), (9, 1), (10, 1), (11, 1), (12, 1), (13, 1), (14, 1), (15, 1), (16, 1), (17, 1), (18, 1), (19, 1)]]"""
+g_dict = corpora.Dictionary([simple_preprocess(line) for line in text])
+g_bow = [g_dict.doc2bow(simple_preprocess(line)) for line in text]
+
+print("Dictionary : ")
+for item in g_bow:
+    print([[g_dict[id], freq] for id, freq in item])
+
+g_tfidf = models.TfidfModel(g_bow, smartirs='ntc')
+print("\n TF-IDF Vector:")
+for item in g_tfidf[g_bow]:
+    print([[g_dict[id], np.around(freq, decimals=2)] for id, freq in item])
+
+##OUTPUT##
+'''
+Output for Bag of Words:
+{'Dog': 0, 'The': 1, 'hat': 2, 'hat.': 3, 'in': 4, 'sat': 5, 'the': 6, 'with': 7}
+Bag of Words :  [[(0, 3), (1, 3), (2, 1), (3, 1), (4, 1), (5, 3), (6, 2), (7, 1)]]
+
+Output for TFIDF:
+Dictionary : 
+[['dog', 3], ['hat', 2], ['in', 1], ['sat', 3], ['the', 5], ['with', 1]]
+
+TF-IDF Vector:
+[['dog', 0.43], ['hat', 0.29], ['in', 0.14], ['sat', 0.43], ['the', 0.71], ['with', 0.14]]
+'''
